d4b9d7b90240d8833ffc498234d863994e4b9dfa
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-03-20 09:33:15 -0300

    Add files via upload

---
+++ exercicio 1 vetores mcom laco de repetição for.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+int main(){
+	
+	int i;
+	int nota[3];
+	
+	for(i=0;i<3;i++){
+		printf("Digiteg a %iª nota: ",i+1);
+		scanf("%i",&nota[i]);
+}
+return 0;
+
+}
